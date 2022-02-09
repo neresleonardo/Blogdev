b185bba41a905f955916eda624e188edc56962c2
Leonardo Borges <leonardo.borges.neres@gmail.com>
2022-02-09 18:53:18 -0300

    render all pagination posts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,19 @@
-import Document from 'next/document';
+import Document, {Html,Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  render() {
+    return(
+      <Html lang="en">
+      <Head>
+        <meta />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Poppins:ital,wght@0,400;1,600&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"></link>
+      </Head>
+      <body>
+          <Main />
+          <NextScript />
+      </body>
+      </Html>
+    )
+  }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { GetStaticProps } from 'next';
-
+import Head from 'next/head'
 import { getPrismicClient } from '../services/prismic';
-
+import Prismic from '@prismicio/client';
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
@@ -17,20 +17,58 @@ interface Post {
 
 interface PostPagination {
   next_page: string;
-  results: Post[];
+  posts: Post[];
 }
 
 interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+ export default function Home() {
+    return (
+      <>
+       <Head>
+        <title>Inicio | Dev Blog</title>
+      </Head>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+      <main>
+        <h1>Ola</h1>
+      </main>
+      </>
+    )
+ }
 
-//   // TODO
-// };
+
+ export const getStaticProps: GetStaticProps = async () => {
+
+const prismic = getPrismicClient();
+
+const postsResponse = await prismic.query([
+  Prismic.predicates.at('document.type', 'postt')
+], {
+  fetch: ['post.title', 'post.content'],
+  pageSize: 20,
+})
+
+const posts = postsResponse.results.map(post => {
+  return {
+    uid: post.uid,
+    data: {
+      title: post.data.title,
+      subtitle: post.data.subtitle,
+      author: post.data.author,
+    },
+    first_publication_date: post.first_publication_date,
+  };
+});
+
+return {
+  props: {
+    postsPagination: {
+      next_page: postsResponse.next_page,
+      results: posts,
+    },
+  },
+  revalidate: 60 * 60 * 24, // 24 hours
+};
+};
